d92748f2a3931b4b18fd15540306c771dbd13e47
Samuel A. Oden <soden2@students.towson.edu>
2016-04-25 11:02:18 -0400

    Added the models and controllers for restaurant, menu, and menu items. Added the database tables for them as well.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+
+
   resource :charges
 
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
   get '/users/auth/twitter/callback', to: 'omniauth_callbacks#twitter'
 
   root 'home#home'
-
+  get '/cart' => 'cart#index'
+  get '/cart/clear' => 'cart#clearCart'
+  get '/cart/:id' => 'cart#add'
 
 end
