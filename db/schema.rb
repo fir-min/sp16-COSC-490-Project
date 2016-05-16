585477222fc4913cb1554c621b101be859fc8af6
yfayng1 <yfayng1@students.towson.edu>
2016-05-16 00:40:04 -0400

    Migrated from sqlite to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,23 @@
 
 ActiveRecord::Schema.define(version: 20160510005207) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "list_id"
+    t.boolean  "done"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "menu_items", force: :cascade do |t|
     t.string   "name"
     t.string   "image"
@@ -54,7 +71,7 @@ ActiveRecord::Schema.define(version: 20160510005207) do
     t.string   "uid"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
