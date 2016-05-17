5a1c312b309a94b5cd62dab14d2e643c5959b907
yfayng1 <yfayng1@students.towson.edu>
2016-05-16 23:07:30 -0400

    Deleted charges.html.erb, Changed deserts to desserts in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ Restaurant.delete_all
 User.delete_all
 a = Restaurant.create(name: 'Firmins Place', logo: 'http://placekitten.com/75/75')
 a_m1 = a.menus.create(name: 'Main')
-a_m2 = a.menus.create(name: 'Deserts')
+a_m2 = a.menus.create(name: 'Desserts')
 a_m3 = a.menus.create(name: 'Kids')
 
 a_m1.menu_items.create(name: 'main 01', price: 5.99,
@@ -26,23 +26,23 @@ a_m1.menu_items.create(name: 'main 05', price: 6.00,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
 
-a_m2.menu_items.create(name: 'desert 01', price: 5.99,
+a_m2.menu_items.create(name: 'dessert 01', price: 5.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-a_m2.menu_items.create(name: 'desert 02', price: 7.99,
+a_m2.menu_items.create(name: 'dessert 02', price: 7.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-a_m2.menu_items.create(name: 'desert 03', price: 2.99,
+a_m2.menu_items.create(name: 'dessert 03', price: 2.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-a_m2.menu_items.create(name: 'desert 04', price: 9.99,
+a_m2.menu_items.create(name: 'dessert 04', price: 9.99,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
 
-a_m2.menu_items.create(name: 'desert 05', price: 6.00,
+a_m2.menu_items.create(name: 'dessert 05', price: 6.00,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
 
@@ -73,7 +73,7 @@ image: 'http://placekitten.com/150/150')
 
 b = Restaurant.create(name: 'Pizza Pizza',  logo: 'http://placekitten.com/75/75')
 b_m1 = b.menus.create(name: 'Main')
-b_m2 = b.menus.create(name: 'Deserts')
+b_m2 = b.menus.create(name: 'Desserts')
 b_m3 = b.menus.create(name: 'Kids')
 b_m4 = b.menus.create(name: 'Drinks')
 
@@ -97,19 +97,19 @@ b_m1.menu_items.create(name: 'main 05', price: 8.00,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
 
-b_m2.menu_items.create(name: 'desert 01', price: 5.25,
+b_m2.menu_items.create(name: 'dessert 01', price: 5.25,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-b_m2.menu_items.create(name: 'desert 02', price: 4.99,
+b_m2.menu_items.create(name: 'dessert 02', price: 4.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-b_m2.menu_items.create(name: 'desert 03', price: 3.99,
+b_m2.menu_items.create(name: 'dessert 03', price: 3.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-b_m2.menu_items.create(name: 'desert 04', price: 9.99,
+b_m2.menu_items.create(name: 'dessert 04', price: 9.99,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
 
@@ -164,7 +164,7 @@ c = Restaurant.create(name: 'Juice', logo: 'http://placekitten.com/75/75')
 c_m1 = c.menus.create(name: 'Juices')
 c_m2 = c.menus.create(name: 'Smoothies')
 c_m3 = c.menus.create(name: 'Sodas')
-c_m4 = c.menus.create(name: 'Deserts')
+c_m4 = c.menus.create(name: 'Desserts')
 
 c_m1.menu_items.create(name: 'juice 01', price: 5.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
@@ -227,30 +227,30 @@ c_m3.menu_items.create(name: 'soda 05', price: 6.00,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
 
-c_m4.menu_items.create(name: 'desert 01', price: 5.99,
+c_m4.menu_items.create(name: 'dessert 01', price: 5.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-c_m4.menu_items.create(name: 'desert 02', price: 7.99,
+c_m4.menu_items.create(name: 'dessert 02', price: 7.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-c_m4.menu_items.create(name: 'desert 03', price: 7.99,
+c_m4.menu_items.create(name: 'dessert 03', price: 7.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-c_m4.menu_items.create(name: 'desert 04', price: 9.99,
+c_m4.menu_items.create(name: 'dessert 04', price: 9.99,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
 
-c_m4.menu_items.create(name: 'desert 05', price: 9.99,
+c_m4.menu_items.create(name: 'dessert 05', price: 9.99,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
 
 
 a = Restaurant.create(name: "Sam's Place", logo: 'http://placekitten.com/75/75')
 a_m1 = a.menus.create(name: 'Main')
-a_m2 = a.menus.create(name: 'Deserts')
+a_m2 = a.menus.create(name: 'Desserts')
 a_m3 = a.menus.create(name: 'Kids')
 
 a_m1.menu_items.create(name: 'main 01', price: 5.99,
@@ -273,23 +273,23 @@ a_m1.menu_items.create(name: 'main 05', price: 6.00,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
 
-a_m2.menu_items.create(name: 'desert 01', price: 5.99,
+a_m2.menu_items.create(name: 'dessert 01', price: 5.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-a_m2.menu_items.create(name: 'desert 02', price: 7.99,
+a_m2.menu_items.create(name: 'dessert 02', price: 7.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-a_m2.menu_items.create(name: 'desert 03', price: 2.99,
+a_m2.menu_items.create(name: 'dessert 03', price: 2.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-a_m2.menu_items.create(name: 'desert 04', price: 9.99,
+a_m2.menu_items.create(name: 'dessert 04', price: 9.99,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
 
-a_m2.menu_items.create(name: 'desert 05', price: 6.00,
+a_m2.menu_items.create(name: 'dessert 05', price: 6.00,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
 
@@ -320,7 +320,7 @@ image: 'http://placekitten.com/150/150')
 
 b = Restaurant.create(name: 'Pasta Madness',  logo: 'http://placekitten.com/75/75')
 b_m1 = b.menus.create(name: 'Main')
-b_m2 = b.menus.create(name: 'Deserts')
+b_m2 = b.menus.create(name: 'Desserts')
 b_m3 = b.menus.create(name: 'Kids')
 b_m4 = b.menus.create(name: 'Drinks')
 
@@ -344,19 +344,19 @@ b_m1.menu_items.create(name: 'main 05', price: 8.00,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
 
-b_m2.menu_items.create(name: 'desert 01', price: 5.25,
+b_m2.menu_items.create(name: 'dessert 01', price: 5.25,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-b_m2.menu_items.create(name: 'desert 02', price: 4.99,
+b_m2.menu_items.create(name: 'dessert 02', price: 4.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-b_m2.menu_items.create(name: 'desert 03', price: 3.99,
+b_m2.menu_items.create(name: 'dessert 03', price: 3.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-b_m2.menu_items.create(name: 'desert 04', price: 9.99,
+b_m2.menu_items.create(name: 'dessert 04', price: 9.99,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
 
@@ -411,7 +411,7 @@ c = Restaurant.create(name: 'Health Club', logo: 'http://placekitten.com/75/75')
 c_m1 = c.menus.create(name: 'Juices')
 c_m2 = c.menus.create(name: 'Smoothies')
 c_m3 = c.menus.create(name: 'Sodas')
-c_m4 = c.menus.create(name: 'Deserts')
+c_m4 = c.menus.create(name: 'Desserts')
 
 c_m1.menu_items.create(name: 'juice 01', price: 5.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
@@ -474,30 +474,30 @@ c_m3.menu_items.create(name: 'soda 05', price: 6.00,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
 
-c_m4.menu_items.create(name: 'desert 01', price: 5.99,
+c_m4.menu_items.create(name: 'dessert 01', price: 5.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-c_m4.menu_items.create(name: 'desert 02', price: 7.99,
+c_m4.menu_items.create(name: 'dessert 02', price: 7.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-c_m4.menu_items.create(name: 'desert 03', price: 7.99,
+c_m4.menu_items.create(name: 'dessert 03', price: 7.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-c_m4.menu_items.create(name: 'desert 04', price: 9.99,
+c_m4.menu_items.create(name: 'dessert 04', price: 9.99,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
 
-c_m4.menu_items.create(name: 'desert 05', price: 9.99,
+c_m4.menu_items.create(name: 'dessert 05', price: 9.99,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
 
 
 a = Restaurant.create(name: 'COSC 490', logo: 'http://placekitten.com/75/75')
 a_m1 = a.menus.create(name: 'Main')
-a_m2 = a.menus.create(name: 'Deserts')
+a_m2 = a.menus.create(name: 'Desserts')
 a_m3 = a.menus.create(name: 'Kids')
 
 a_m1.menu_items.create(name: 'main 01', price: 5.99,
@@ -520,23 +520,23 @@ a_m1.menu_items.create(name: 'main 05', price: 6.00,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
 
-a_m2.menu_items.create(name: 'desert 01', price: 5.99,
+a_m2.menu_items.create(name: 'dessert 01', price: 5.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-a_m2.menu_items.create(name: 'desert 02', price: 7.99,
+a_m2.menu_items.create(name: 'dessert 02', price: 7.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-a_m2.menu_items.create(name: 'desert 03', price: 2.99,
+a_m2.menu_items.create(name: 'dessert 03', price: 2.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-a_m2.menu_items.create(name: 'desert 04', price: 9.99,
+a_m2.menu_items.create(name: 'dessert 04', price: 9.99,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
 
-a_m2.menu_items.create(name: 'desert 05', price: 6.00,
+a_m2.menu_items.create(name: 'dessert 05', price: 6.00,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
 
@@ -567,7 +567,7 @@ image: 'http://placekitten.com/150/150')
 
 b = Restaurant.create(name: 'Falafel House',  logo: 'http://placekitten.com/75/75')
 b_m1 = b.menus.create(name: 'Main')
-b_m2 = b.menus.create(name: 'Deserts')
+b_m2 = b.menus.create(name: 'Desserts')
 b_m3 = b.menus.create(name: 'Kids')
 b_m4 = b.menus.create(name: 'Drinks')
 
@@ -591,19 +591,19 @@ b_m1.menu_items.create(name: 'main 05', price: 8.00,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
 
-b_m2.menu_items.create(name: 'desert 01', price: 5.25,
+b_m2.menu_items.create(name: 'dessert 01', price: 5.25,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-b_m2.menu_items.create(name: 'desert 02', price: 4.99,
+b_m2.menu_items.create(name: 'dessert 02', price: 4.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-b_m2.menu_items.create(name: 'desert 03', price: 3.99,
+b_m2.menu_items.create(name: 'dessert 03', price: 3.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-b_m2.menu_items.create(name: 'desert 04', price: 9.99,
+b_m2.menu_items.create(name: 'dessert 04', price: 9.99,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
 
@@ -658,7 +658,7 @@ c = Restaurant.create(name: "Fima's Juice Bar", logo: 'http://placekitten.com/75
 c_m1 = c.menus.create(name: 'Juices')
 c_m2 = c.menus.create(name: 'Smoothies')
 c_m3 = c.menus.create(name: 'Sodas')
-c_m4 = c.menus.create(name: 'Deserts')
+c_m4 = c.menus.create(name: 'Desserts')
 
 c_m1.menu_items.create(name: 'juice 01', price: 5.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
@@ -721,22 +721,22 @@ c_m3.menu_items.create(name: 'soda 05', price: 6.00,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
 
-c_m4.menu_items.create(name: 'desert 01', price: 5.99,
+c_m4.menu_items.create(name: 'dessert 01', price: 5.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-c_m4.menu_items.create(name: 'desert 02', price: 7.99,
+c_m4.menu_items.create(name: 'dessert 02', price: 7.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-c_m4.menu_items.create(name: 'desert 03', price: 7.99,
+c_m4.menu_items.create(name: 'dessert 03', price: 7.99,
 description: 'Lorem ipsum dolor sit amet, quod copiosae per ne',
 image: 'http://placekitten.com/150/150')
 
-c_m4.menu_items.create(name: 'desert 04', price: 9.99,
+c_m4.menu_items.create(name: 'dessert 04', price: 9.99,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
 
-c_m4.menu_items.create(name: 'desert 05', price: 9.99,
+c_m4.menu_items.create(name: 'dessert 05', price: 9.99,
 description: 'Est adipisci liberavisse eu, mea melius iisque omittantur eu.',
 image: 'http://placekitten.com/150/150')
